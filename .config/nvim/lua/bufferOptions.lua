1946504095bd681ace3063499f18b81196c549dd
Gnarks <231026@umons.ac.be>
2025-03-03 16:39:22 +0100

    changed to shift (easier navigation) and go to prev on delete

--- .config/nvim/lua/bufferOptions.lua
+++ .config/nvim/lua/bufferOptions.lua
@@ -1,4 +1,4 @@
 local opt = { noremap = true, silent = true }
-vim.keymap.set('n', '<leader>j', function() vim.cmd('bprevious') end, opt)
-vim.keymap.set('n', '<leader>k', function() vim.cmd('bnext') end, opt)
-vim.keymap.set('n', '<leader>x', function() vim.cmd('bd') end, opt)
+vim.keymap.set('n', '<S-j>', function() vim.cmd('bprevious') end, opt) -- move to the previous buffer with shift-j 
+vim.keymap.set('n', '<S-k>', function() vim.cmd('bnext') end, opt) -- move to next buffer with shift-k
+vim.keymap.set('n', '<S-x>', function() vim.cmd('bd | bprevious') end, opt) -- closes the current buffer with shift-x
