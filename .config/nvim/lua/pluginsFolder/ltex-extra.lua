1460113a7d6848ac22fff338ac4ed7d7ac54f6a9
Gnarks <231026@umons.ac.be>
2025-04-12 15:48:58 +0200

    fixed errors on opening latex files

--- .config/nvim/lua/pluginsFolder/ltex-extra.lua
+++ .config/nvim/lua/pluginsFolder/ltex-extra.lua
@@ -1,21 +1,22 @@
-return {
-    "barreiroleo/ltex_extra.nvim",
-    ft = { "markdown", "tex" },
-    dependencies = {"neovim/nvim-lspconfig"},
 
+return {
+  "barreiroleo/ltex_extra.nvim",
+  ft = { "markdown", "tex" },
+  dependencies = { "neovim/nvim-lspconfig" },
+  -- yes, you can use the opts field, just I'm showing the setup explicitly
   config = function()
     require("ltex_extra").setup {
-      load_langs = { "en-US", "fr" }, -- en-US as default
-      -- boolean : whether to load dictionaries on startup
-      init_check = true,
-      -- string : relative or absolute path to store dictionaries
-      -- e.g. subfolder in the project root or the current working directory: ".ltex"
-      -- e.g. shared files for all projects:  vim.fn.expand("~") .. "/.local/share/ltex"
+      load_langs = { "en-GB", "fr" }, -- languages for witch dictionaries will be loaded
+      init_check = true, -- whether to load dictionaries on startup
       path = "", -- project root or current working directory
-      log_level = "none",
-      -- table : configurations of the ltex language server.
-      -- Only if you are calling the server from ltex_extra
-      server_opts = nil
+      log_level = "none", -- "none", "trace", "debug", "info", "warn", "error", "fatal"
+
+      -- need to keep this in to not get errors from the ltex attach
+      server_opts = {
+        on_attach = function(client, bufnr)
+          -- your on_attach process
+        end,
+      },
     }
   end
 }
