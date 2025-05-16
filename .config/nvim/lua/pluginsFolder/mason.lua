6b2e58e8465fa8a4844783c00bda2a8187ffbb2b
Gnarks <231026@umons.ac.be>
2025-05-16 08:37:20 +0200

    updating Lazy packets with breaking change in lsp-config

--- .config/nvim/lua/pluginsFolder/mason.lua
+++ .config/nvim/lua/pluginsFolder/mason.lua
@@ -1,23 +1,13 @@
-return   {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        -- Asking for the mason;nvim plugin to be loaded specificaly before 
-        dependencies = { "mason.nvim" },
-        config = function()
-            require("mason-lspconfig").setup()
-
-            -- setting up all lsps installed with mason
-            require("mason-lspconfig").setup_handlers({
-                function (server_name)
-                    require("lspconfig")[server_name].setup({ settings = {signatureHelp = {enable = true}; }})
-                end,
-            })
-        end,
-    },
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    -- Asking for the mason.nvim plugin to be loaded specificaly before
+    dependencies = { "williamboman/mason.nvim" },
+  },
 }
